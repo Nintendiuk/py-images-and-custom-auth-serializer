e7abed2818b62a8893dc8612be35043be2252fb1
Nintendiuk <oleksii.rachynskyi@gmail.com>
2026-05-31 17:04:20 +0300

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -43,7 +43,7 @@ class Actor(models.Model):
 def movie_image_file_path(instance, filename):
     """Generate file path for a new movie image."""
     ext = os.path.splitext(filename)[1]
-    filename = f"{slugify(instance.title)}--{uuid.uuid4()}{ext}"
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{ext}"
     return os.path.join("uploads", "movies", filename)
 
 
